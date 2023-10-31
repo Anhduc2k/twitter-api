ecd2f543b5011835a01cf402da34e2729ae483ac
Eng Ger <duclapulga@gmail.com>
2023-10-31 22:01:19 +0700

    create user schema

--- src/controllers/users.controllers.ts
+++ src/controllers/users.controllers.ts
@@ -1,13 +1,34 @@
-import { Request, Response } from "express";
+import { Request, Response } from 'express'
+import User from '~/models/schemas/User.schema'
+import databaseService from '~/services/database.services'
+import usersService from '~/services/user.services'
 
 export const loginController = (req: Request, res: Response) => {
-  const { email, password } = req.body;
-  if (email === "abc@gmail.com" && password === "123") {
+  const { email, password } = req.body
+  if (email === 'abc@gmail.com' && password === '123') {
     return res.status(200).json({
-      message: "Login success"
+      message: 'Login success'
     })
   }
   return res.status(400).json({
-    message: "Login fail"
+    message: 'Login fail'
   })
+}
+
+export const registerController = async (req: Request, res: Response) => {
+  const { email, password } = req.body
+
+  try {
+    const result = await usersService.register({ email, password})
+    return res.json({
+      message: "Register success",
+      result
+    })
+  } catch (error) {
+    console.log(error);
+    return res.status(400).json({
+      message: 'Registration failed',
+      error
+    })
+  }
 }
--- src/services/database.services.ts
+++ src/services/database.services.ts
@@ -1,27 +1,36 @@
-import { MongoClient } from 'mongodb';
+
+import { Collection, Db, MongoClient } from 'mongodb';
 import { config } from 'dotenv';
+import User from '~/models/schemas/User.schema';
 
 config()
 const uri = `mongodb+srv://${process.env.DB_USERNAME}:${process.env.DB_PASSWORD}@twitter.cfcgfp5.mongodb.net/?retryWrites=true&w=majority`
 
 class DatabaseService {
   private client: MongoClient
+  private db: Db
   constructor() {
     this.client = new MongoClient(uri)
+    this.db = this.client.db(process.env.DB_NAME)
   }
 
   async connect() {
     try {
-      await this.client.db('admin').command({ ping: 1 })
+      await this.db.command({ ping: 1 })
       console.log('Pinged your deployment. You successfully connected to MongoDB!')
-    } finally {
-      // Ensures that the client will close when you finish/error
-      await this.client.close()
+    } catch (err) {
+      console.log('Error', err)
+      throw err
     }
+  }
+
+  get users(): Collection<User> {
+    return this.db.collection(process.env.DB_USER_COLLECTION as string)
   }
 }
 
 // Tao object tu class DatabaseService
+
 
 const databaseService = new DatabaseService();
 export default databaseService
